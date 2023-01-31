288ede51bd39ad7e1e36b8036ca9e2147f7de910
howard <Busta@live.ru>
2023-02-01 03:44:41 +0700

    installation of traffic lights and buttons

--- TrafficLightV1/ViewController.swift
+++ TrafficLightV1/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        
     }
 
-
+    @IBAction func startButtonDidTab() {
+        if redLightView.alpha == 0.3 {
+            startButton.setTitle ("Start", for: .normal)
+        } else {
+                startButton.setTitle("Next", for: .normal)
+                redLightView.alpha = 1
+            }
+            
+        }
+    
 }
+/*
+я думаю нужно было использовать switch,так как количество нажатий неизвестно. Честно сознаюсь не успел попробовать все варианты. Оправдываться конечно бесполезно, но дети и семейные проблемы  сжирают много времени.
+*/
 
