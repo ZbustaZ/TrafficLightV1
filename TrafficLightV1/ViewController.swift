da183ffba74ee40d7e055c9f50c89691162ff58a
howard <Busta@live.ru>
2023-01-31 11:43:22 +0700

    Initial Commit

---
+++ TrafficLightV1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightV1
+//
+//  Created by Howard Matthews on 31.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
